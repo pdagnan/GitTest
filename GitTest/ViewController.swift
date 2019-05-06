8ba9cd154fcc5061659c728594c02abe7f0aeb8a
Philip Dagnan <philipdagnan@gmail.com>
2019-05-06 11:28:59 +0100

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Philip Dagnan on 06/05/2019.
+//  Copyright © 2019 Philip Dagnan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
